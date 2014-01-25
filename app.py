27b3c2d3367d85596208bea7d72abb88a1440028
Jay Reddy <jay.w.reddy@gmail.com>
2014-01-24 23:25:03 -0800

    added home

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import bottle
 import os
+
  
 @bottle.route('/')
 def index():
     return "Hello World"
+
+@bottle.route("/home")
+def home():
+    return 2+2
 
 bottle.run(host = '0.0.0.0', port=os.environ.get('PORT', 5000))
